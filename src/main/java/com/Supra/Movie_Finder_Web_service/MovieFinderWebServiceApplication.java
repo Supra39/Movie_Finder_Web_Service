d936f39658fc14bf54402a4dceda174931ea3a23
Supra39 <erik.7337@gmail.com>
2024-10-23 19:57:42 +0200

    Initial commit

---
+++ src/main/java/com/Supra/Movie_Finder_Web_service/MovieFinderWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Supra.Movie_Finder_Web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFinderWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFinderWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Supra/Movie_Finder_Web_service/MovieFinderWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Supra.Movie_Finder_Web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFinderWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
